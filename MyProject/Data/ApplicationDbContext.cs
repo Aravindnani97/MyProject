9a32a8f937d77d69b1c0e3c20d1d150a59894ef2
Aravindnani97 <aravind1123045@gmail.com>
2020-09-29 12:31:25 +0530

    Created a Product migration and database

--- MyProject/Data/ApplicationDbContext.cs
+++ MyProject/Data/ApplicationDbContext.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyProject.Models;
+using MyProject.Data.Configurations;
 
 namespace MyProject.Data
 {
@@ -11,6 +13,14 @@ namespace MyProject.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProductConfiguration()).SeedData();
+            // ModelBuilderExtension.SeedData(builder);
+            base.OnModelCreating(builder);
         }
     }
 }
